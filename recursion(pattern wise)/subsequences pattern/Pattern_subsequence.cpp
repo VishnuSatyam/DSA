42331dfac804011a9682570dbed6a058eed6ebe0
Vishnu Satyam <pandeyvishnusatyam@gmail.com>
2026-04-28 22:06:40 +0530

    added new backtracking problems

--- recursion(pattern wise)/subsequences pattern/Pattern_subsequence.cpp
+++ recursion(pattern wise)/subsequences pattern/Pattern_subsequence.cpp
@@ -37,3 +37,48 @@ int main() {
 
     return 0;
 }
+
+
+
+// if we just need to print one suubsequence equal to sum 
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool printS(int ind, vector<int>& ds, int currSum, int target, int arr[], int n) {
+    
+    if (ind == n) {
+        if (currSum == target) {
+            for (int x : ds) cout << x << " ";
+            cout << endl;
+            return true; // ✅ stop everything
+        }
+        return false;
+    }
+
+    // take
+    ds.push_back(arr[ind]);
+    if (printS(ind + 1, ds, currSum + arr[ind], target, arr, n))
+        return true;
+
+    // backtrack
+    ds.pop_back();
+
+    // not take
+    if (printS(ind + 1, ds, currSum, target, arr, n))
+        return true;
+
+    return false;
+}
+
+int main() {
+    int arr[] = {1, 2, 1};
+    int n = 3;
+    int target = 2;
+
+    vector<int> ds;
+    printS(0, ds, 0, target, arr, n);
+
+    return 0;
+}
